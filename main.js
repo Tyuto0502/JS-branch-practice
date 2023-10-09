bf96af0ded4aec151210d7c13ee41cfdc665224f
Yuto Tamura <cayt1224@gmail.com>
2023-10-09 20:51:52 +0900

    課題作成

---
+++ main.js
@@ -0,0 +1,8 @@
+var text = "elseif-practice";
+if (text === "elseif-practice") {
+  console.log (text);
+} else if (text === "else-practice2") {
+  console.log ("少し違う");
+} else {
+  console.log ("その他");
+}
